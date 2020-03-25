75624e843d64f6ad594d47c29e12e019bad635c5
v-almonacid <ealmonacid@gmail.com>
2020-03-25 14:39:09 +0100

    do not use Cocoapods for building on iOS
    I removed all the dependencies in the podfile so that everything is configured manually in the xcode project---exactly as it was before the upgrade.
    Added @react-native-community/geolocation because it was removed from RN core and for some reason it's also a dependency in iOS (though I don't reallt know whjy it is required). Both Android and iOS should be correctly building at this point

--- android/app/src/main/java/com/emurgo/MainApplication.java
+++ android/app/src/main/java/com/emurgo/MainApplication.java
@@ -4,21 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-// import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-// import io.emurgo.chainlibs.ChainLibsPackage;
-// import com.learnium.RNDeviceInfo.RNDeviceInfo;
-// import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-// import io.sentry.RNSentryPackage;
-// import com.igorbelyayev.rnlocalresource.RNLocalResourcePackage;
-// import org.devio.rn.splashscreen.SplashScreenReactPackage;
-// import com.ocetnik.timer.BackgroundTimerPackage;
-// import io.crossroad.rncardano.CardanoPackage;
-// import org.reactnative.camera.RNCameraPackage;
-// import com.bitgo.randombytes.RandomBytesPackage;
-// import com.rnfs.RNFSPackage;
-// import com.BV.LinearGradient.LinearGradientPackage;
-// import com.horcrux.svg.SvgPackage;
-
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -43,24 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       // packages.add(new ChainLibsPackage());
       // packages.add(new CardanoPackage());
       packages.add(new KeyStorePackage());
-      // packages.add(new ReactNativeConfigPackage());
       return packages;
-
-        // new ChainLibsPackage(),
-        // new RNDeviceInfo(),
-        // new AsyncStoragePackage(),
-        // new RNSentryPackage(),
-        // new RNLocalResourcePackage(),
-        // new ReactNativeConfigPackage(),
-        // new SplashScreenReactPackage(),
-        // new CardanoPackage(),
-        // new RNCameraPackage(),
-        // new RandomBytesPackage(),
-        // new RNFSPackage(),
-        // new LinearGradientPackage(),
-        // new SvgPackage(),
-        // new KeyStorePackage(),
-        // new BackgroundTimerPackage()
     }
 
     @Override
